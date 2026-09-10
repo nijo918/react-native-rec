83572735747505ff0a4eedc7beca4f5cf54968fb
Nigel <nigelchimanikire09@gmail.com>
2026-09-10 18:17:54 +0200

    Initial commit

---
+++ app/(auth)/sign_up.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+import {Link} from "expo-router";
+
+const SignUp = () => {
+    return (
+        <View>
+            <Text>SignUp</Text>
+            <Link href="/(auth)/sign_in">Sign in</Link>
+        </View>
+    )
+}
+export default SignUp
---
+++ app/(tabs)/subscriptions.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+import {Stack} from "expo-router";
+
+const Subscriptions = () =>{
+    return(
+        <View>
+          <Text>Subscriptions</Text>
+        </View>
+    )
+}
+export default Subscriptions
